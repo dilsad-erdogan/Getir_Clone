2778c31c79acb8e44bc432bd8ccbef58faaa9d29
Dilsad Rukiye Erdogan <dilsadrukiyeerdogan@gmail.com>
2026-05-04 21:46:03 +0300

    banner

--- GetirClone/src/screens/HomeScreen/index.tsx
+++ GetirClone/src/screens/HomeScreen/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { ScrollView } from "react-native"
 import HeaderMain from "../../components/HeaderMain"
+import BannerCarousel from "../../components/BannerCarousel"
 
 function index() {
   return (
-    <ScrollView>
+    <ScrollView stickyHeaderIndices={[0]} style={{backgroundColor:'#f5f5f5'}}>
         <HeaderMain />
+        <BannerCarousel />
     </ScrollView>
   )
 }
